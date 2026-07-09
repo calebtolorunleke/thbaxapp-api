7a713fab530a510fd64a13dd64a7aca5952b557c
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-09 13:33:16 -0700

    refactor api routes and the JSON middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
+const homeRoute = require("../src/routes/homeRoute");
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello word!");
-});
+app.get("/", homeRoute);
 
 app.get("/api", (req, res) => {
   res.json({
